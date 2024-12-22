2089d0dda38ab050dd801250516e29258a9873df
Pavellow <polo2b1.pav@gmail.com>
2024-12-22 18:14:05 +0100

    fix auth front

--- frontend/mobile/lib/config/environment.dart
+++ frontend/mobile/lib/config/environment.dart
@@ -9,7 +9,7 @@ class EnvironmentConfig {
     _environment = env;
     switch (_environment) {
       case Environment.dev:
-        _apiUrl = 'http://0.0.0.0:8000';
+        _apiUrl = 'http://10.0.2.2:8000';
         break;
       case Environment.staging:
         _apiUrl = 'https://staging-api.edf.fr';
